e73e1a838b5457c96bb7816a32f5e1e681b6bad4
Gilbert Elikplim Kukah <kwamegilbert1114@gmail.com>
2025-02-07 18:27:10 +0000

    Models set

--- slim-php/public/index.php
+++ slim-php/public/index.php
@@ -4,18 +4,20 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
-require __DIR__ . '/../vendor/autoload.php';
+// require __DIR__ . '/../helpers/Bootstrap.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../config/database.php';
+require_once __DIR__ . '/../helpers/ErrorHandler.php';
 
 $app = AppFactory::create();
 
+$app->addErrorMiddleware(true, true, true,)->setDefaultErrorHandler(new ErrorHandler());
+
 $app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
+    $response->getBody()->write("Welcome to my Slim App");
     return $response;
 });
 
-$app->addErrorMiddleware(false, true, true);
 $app->setBasePath("/room-vibe-backend/slim-php");
 
 $app->run();
-
-
