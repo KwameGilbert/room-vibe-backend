cef27656b25097c0122c2498f4e08aec24781209
Kwame Gilbert <kwamegilbert1114@gmail.com>
2025-03-13 11:26:45 +0000

    Route working on

--- src/routes/php/BookingRoute.php
+++ src/routes/php/BookingRoute.php
@@ -11,11 +11,14 @@ return function ($app){
         $student_id = $args['student_id'];
         $data = json_decode($request->getBody()->getContents(), true);
         $result = $bookingController->createBooking($student_id, $data);
+        $response->getBody()->write($result);
+        return $response->withHeader('Content-Type', 'application/json');
     });
 
-    $app->get('/api/booking/{booking_id}', function ($request, $response, $args) use ($bookingController){
+    $app->get('/api/booking/{booking_id}', function ($request, $response, $args) use ($bookingController) {
         $booking_id = $args['booking_id'];
-        $result = $bookingController->getBookingById($booking_id); 
-        
-    })
+        $result = $bookingController->getBookingById($booking_id);
+        $response->getBody()->write($result);
+        return $response->withHeader('Content-Type', 'application/json');
+    });
 };
