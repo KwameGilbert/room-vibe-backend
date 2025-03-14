8189b5c8db5c1a3703b82a932c83011588134c39
Gilbert Elikplim Kukah <kwamegilbert1114@gmail.com>
2025-03-14 18:28:08 +0000

    AmenityRoute.php

--- src/routes/php/AmenityRoute.php
+++ src/routes/php/AmenityRoute.php
@@ -3,6 +3,7 @@
 namespace App\Route;
 
 use App\Controller\AmenityController;
+require_once __DIR__ . '/../../controller/AmenityController.php';
 
 return function ($app) {
     $amenityController = new AmenityController();
