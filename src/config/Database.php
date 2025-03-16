9f8b9c82365b63d4e32875c93c522226b7e2f658
Gilbert Elikplim Kukah <kwamegilbert1114@gmail.com>
2025-03-16 01:53:03 +0000

    Remove dotenv loading from Database.php and simplify PDO connection string

--- src/config/Database.php
+++ src/config/Database.php
@@ -3,9 +3,6 @@
 require_once __DIR__ . '/../../vendor/autoload.php';
 require_once __DIR__ . '/../helpers/LoggerFactory.php';
 
-$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
-$dotenv->load();
-
 class Database
 {
 
@@ -33,7 +30,7 @@ class Database
 
         try {
             $this->conn = new \PDO(
-                "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
+                "mysql:host={$this->host};dbname={$this->db_name}",
                 $this->username,
                 $this->password
             );
