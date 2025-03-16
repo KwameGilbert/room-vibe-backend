4dd7cf7c928492e208c3fc358635d70a9cecadd8
Gilbert Elikplim Kukah <kwamegilbert1114@gmail.com>
2025-03-16 12:58:44 +0000

    Uncomment base path setting in index.php

--- public/index.php
+++ public/index.php
@@ -27,7 +27,7 @@ AppFactory::setContainer($container);
 $app = AppFactory::create();
 
 // Set Base Path from environment variable (if not set, default to an empty string)
-// $app->setBasePath($_ENV['BASE_PATH'] ?? '');
+$app->setBasePath($_ENV['BASE_PATH'] ?? '');
 
 // In production, consider setting displayErrorDetails to false.
 $app->addErrorMiddleware(
