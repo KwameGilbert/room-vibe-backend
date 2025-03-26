f84ba5e31fa23153b68b7cd365482b189845020b
Gilbert Elikplim Kukah <kwamegilbert1114@gmail.com>
2025-03-26 06:24:06 +0000

    2025-03-26 06:24:05

--- frontend/index.php
+++ frontend/index.php
@@ -24,15 +24,15 @@ $student_id = $_SESSION['student_id'];
 
     <script>
     // Check if Service Workers are supported
-    if ('serviceWorker' in navigator) {
-        window.addEventListener('load', () => {
-            navigator.serviceWorker.register('./service-worker.min.js').then((registration) => {
-                console.log('Service Worker registered successfully with scope: ', registration.scope);
-            }).catch((error) => {
-                console.log('Service Worker registration failed: ', error);
-            });
-        });
-    };
+    // if ('serviceWorker' in navigator) {
+    //     window.addEventListener('load', () => {
+    //         navigator.serviceWorker.register('./service-worker.min.js').then((registration) => {
+    //             console.log('Service Worker registered successfully with scope: ', registration.scope);
+    //         }).catch((error) => {
+    //             console.log('Service Worker registration failed: ', error);
+    //         });
+    //     });
+    // };
     </script>
 </body>
 
